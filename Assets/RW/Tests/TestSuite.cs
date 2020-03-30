88e023151d77fd318cca4481efc554280d3e7da1
Lenovo <77ripdrive@gmail.com>
2020-03-30 17:57:54 +0300

    init Unity test project

---
+++ Assets/RW/Tests/TestSuite.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        private Game game;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator AsteroidsMoveDown()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            float initialYPos = asteroid.transform.position.y;
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.Less(asteroid.transform.position.y, initialYPos);
+        }
+
+        [UnityTest]
+        public IEnumerator GameOverOccursOnAsteroidCollision()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = game.GetShip().transform.position;
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.True(game.isGameOver);
+        }
+
+        [UnityTest]
+        public IEnumerator NewGameRestartsGame()
+        {
+            game.isGameOver = true;
+            game.NewGame();
+            Assert.False(game.isGameOver);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator LaserMovesUp()
+        {
+            GameObject laser = game.GetShip().SpawnLaser();
+            float initialYPos = laser.transform.position.y;
+            yield return new WaitForSeconds(0.1f);
+            Assert.Greater(laser.transform.position.y, initialYPos);
+        }
+
+        [UnityTest]
+        public IEnumerator LaserDestroysAsteroid()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            UnityEngine.Assertions.Assert.IsNull(asteroid);
+        }
+
+        [UnityTest]
+        public IEnumerator DestroyedAsteroidRaisesScore()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            Assert.AreEqual(game.score, 1);
+        }
+
+
+    }
+}
